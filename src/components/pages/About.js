3359343a64ba807975d37ad2aeaffc09db362508
Caliza <rances.rodriguez@gmail.com>
2023-04-28 15:52:00 -0700

    Added styling and images to About.js.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import img1 from '../images/passwordGenerator.jpg';
 
 export default function About() {
   return (
     <div>
       <h1>Portfolio</h1>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+        }}>
+        <div
+        style={{
+          margin: "15px"
+        }}>
+          <a href={url}><img src={img1} width="400" height="400" /></a>
+          
+        </div>
+        <div
+        style={{
+          margin: "15px"
+        }}>
+          <img src={img1} width="400" height="400" />
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%"
+        }}>
+        <div
+        style={{
+          margin: "15px"
+        }}>
+          <img src={img1} width="400" height="400" />
+        </div>
+        <div
+        style={{
+          margin: "15px"
+        }}>
+          <img src={img1} width="400" height="400" />
+        </div>
+      </div>
+
       <p>
         Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
         Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
